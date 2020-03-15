c64b7e391802569563c282cf7e6efee7a906ac68
lovetanpopo <love10and_love22@hotmail.com>
2020-03-15 23:27:24 +0900

    oh...

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const server = new ApolloServer({
   }
 });
 
-server.listen({ port: process.env.PORT | 4000 }).then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
   console.log(process.env);
 });
