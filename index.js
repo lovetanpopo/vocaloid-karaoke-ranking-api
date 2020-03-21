7de6885a63d67cd0e261d086a5edfa6398e2cb0f
lovetanpopo <love10and_love22@hotmail.com>
2020-03-21 17:58:53 +0900

    DAM対応

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { AuthenticationError } = require("apollo-server-errors");
-const { ApolloServer, gql } = require('apollo-server');
+const {AuthenticationError} = require("apollo-server-errors");
+const {ApolloServer, gql} = require('apollo-server');
 const osmosis = require('osmosis');
 require('dotenv').config();
 
@@ -11,20 +11,46 @@ const typeDefs = gql`
   }
 
   type Query {
-    ranking: [Info]
+    ranking(dataSource: DataSource): [Info]
+  }
+
+  enum DataSource {
+    KARATETSU
+    DAM
   }
 `;
 
-const getKaratetsuRanking = () => {
+const settings = {
+  KARATETSU: {
+    url: 'https://www.karatetsu.com/vocala/pickup/ranking.php',
+    find: '.pickuplist > tbody > tr',
+    set: {
+      title: 'td[2] > a',
+      author: 'td[3] > span',
+      rank: 'td[1]'
+    }
+  },
+  DAM: {
+    url: 'https://www.clubdam.com/app/dam/ranking/vocaloid-monthly.html',
+    find: '.ranking-item',
+    set: {
+      title: '.title > p > span',
+      author: '.artist > p',
+      rank: '.rank > span'
+    }
+  }
+};
+
+const getRanking = (setting) => {
   return new Promise(resolve => {
     const results = [];
     osmosis
-      .get('https://www.karatetsu.com/vocala/pickup/ranking.php')
-      .find('.pickuplist > tbody > tr')
+      .get(setting.url)
+      .find(setting.find)
       .set({
-        title: 'td[2] > a',
-        author: 'td[3] > span',
-        rank: 'td[1]'
+        title: setting.set.title,
+        author: setting.set.author,
+        rank: setting.set.rank
       })
       .data(item => results.push(item))
       .done(() => resolve(results));
@@ -33,21 +59,23 @@ const getKaratetsuRanking = () => {
 
 const resolvers = {
   Query: {
-    ranking: async () => await getKaratetsuRanking()
+    ranking: async (parent, args, _context, _info) =>
+      await getRanking(settings[args.dataSource ? args.dataSource : 'KARATETSU'])
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => {
-    const env = process.env.AUTHORIZATION;
-    const token = req.headers.authorization || '';
-    if (env !== token) throw new AuthenticationError('authentication failed');
+  context: ({req}) => {
+    if (process.env.NODE_ENV !== 'development') {
+      const env = process.env.AUTHORIZATION;
+      const token = req.headers.authorization || '';
+      if (env !== token) throw new AuthenticationError('authentication failed');
+    }
   }
 });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+server.listen({port: process.env.PORT || 4000}).then(({url}) => {
   console.log(`🚀  Server ready at ${url}`);
-  console.log(process.env);
 });
